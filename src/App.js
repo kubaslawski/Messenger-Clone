956081ed2de3114836c6468f699829f1bd6fa62f
Kuba Ślawski <kubaslawski@gmail.com>
2020-11-27 14:58:24 +0100

    creating state, messages input, rendering messages, using material UI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-
+import react, {useState} from "react";
 import './App.css';
+import { Button, FormControl, InputLabel, Input} from '@material-ui/core';
 
 function App() {
+
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  console.log(input)
+  
+  const sendMessage = e => {
+    e.preventDefault();
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
+      <form>
+      <FormControl>
+        <InputLabel >Enter a message...</InputLabel>
+        <Input value={input} onChange={e => setInput(e.target.value)}/>
+        <Button disabled={!input} variant="outlined" color="primary" type="submit" onClick={sendMessage}>Send Message</Button>
+      </FormControl>
+      </form>
+
+      {messages.map(message => {
+        return (
+          <p key={message.id}>{message}</p>
+        )
+      })}
     </div>
   );
 }
